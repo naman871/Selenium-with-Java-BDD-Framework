cc6dc817625898dec6366db9301c2d0f63258615
NAMAN GUPTA <namangupta871@gmail.com>
2022-12-05 12:23:57 +0530

    removed unwanted stepdefinition

--- src/test/java/stepdefinitions/OpportunitiesStepdefinition.java
+++ src/test/java/stepdefinitions/OpportunitiesStepdefinition.java
@@ -103,11 +103,6 @@ public class OpportunitiesStepdefinition {
         opportunitiesSteps.clickOnViewOpportunities();
     }
 
-    @Then("enter mandatory fields like opportunity name {string} sales stage {string}")
-    public void enterManditoryFieldsUnderOpportunity(String opportunityName , String salesStageName) {
-        opportunitiesSteps.enterDetailsToCreateOpportunity(opportunityName,salesStageName);
-    }
-
     @Then("Select checkbox beside {string} dropdown")
     public void selectCheckboxBesideBulkActionDropdown() {
         opportunitiesSteps.selectAllCheckbox();
