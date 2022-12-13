4a7366025ee5d368a82f6e888008008325fabc2c
CSQA <CSQA@LAPTOP-IDN1KLM4>
2022-12-13 12:32:23 +0530

    Adding Lead and opportunities code by ramya jami

--- src/test/java/runner/CucumberTestRunner.java
+++ src/test/java/runner/CucumberTestRunner.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.WebDriver;
         features = "src/test/resources/features",
         glue = {"stepdefinitions"},
         monochrome = true,
-        tags = "@log11",
+        tags = "@TC_028",
         plugin = {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "html:target/cucumber-html-report",
